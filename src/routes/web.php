0ed4fb1edea4ec11f16ca5b75dc97f16686e45de
Zvezdan Nedimovic <zvezdann11@gmail.com>
2024-07-27 14:32:55 +0200

    dashboard to admin route rename

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('/admin', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
